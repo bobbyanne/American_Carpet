ad2abe657ea21ebb686fce7e68f6141b2a2647fc
bobbyanne <bobbyanne92@gmail.com>
2017-10-05 14:23:32 -0500

    Fixed some bugs and google map redirects

--- resources/common/js/common.js
+++ resources/common/js/common.js
@@ -1,5 +1,5 @@
 
-$(function() {
+$(function () {
     var $menus = $('.menu');
     var $subMenus = $('.sub-menu');
     var $mobileMenu = $('#mobile-menu');
@@ -7,7 +7,7 @@ $(function() {
     var $scrollTop = $('#scroll-to-top');
 
     /*``````````````````````````````````````````````````````````````````*/
-                            /* HELPER FUNCTIONS */
+    /* HELPER FUNCTIONS */
     function closeMobileMenu(animate) {
         animate = animate === true ? true : false;
         $mobileMenuToggle.children().removeClass('clicked');
@@ -17,21 +17,31 @@ $(function() {
             $mobileMenu.css('display', 'none');
         }
     }
-    
+
     /*``````````````````````````````````````````````````````````````````*/
 
-    $menus.click(function() {
+    $(window).click(function (e) {
+        // Checking to see if a menu is open and if it is, then close it
+        var $target = $(e.target);
+        if ($target.hasClass('sub-menu') === false && $target.hasClass('menu') === false) {
+            var $subMenu = $subMenus.filter('.show');
+            $subMenu.removeClass('show');
+            $subMenu.parent().removeClass('selected');
+        }
+    });
+
+    $menus.click(function () {
         var $this = $(this);
         $this.toggleClass('selected');
         $menus.not($this).removeClass('selected');
 
         var $subMenu = $this.children('.sub-menu');
 
-        $('li', $this).mouseenter(function() {
+        $('li', $this).mouseenter(function () {
             $('img', $(this)).stop(true, true).animate({
                 opacity: 1
             }, 100);
-        }).mouseleave(function(){
+        }).mouseleave(function () {
             $('img', $(this)).stop(true, true).animate({
                 opacity: 0
             }, 100);
@@ -39,7 +49,7 @@ $(function() {
 
         $subMenus.not($subMenu).removeClass('show');
         $subMenu.toggleClass('show');
-        $subMenus.each(function() {
+        $subMenus.each(function () {
             var $this = $(this);
             if (!$(this).hasClass('show')) {
                 $('li', $this).removeClass('show');
@@ -52,28 +62,28 @@ $(function() {
         }
     });
 
-    $mobileMenuToggle.click(function() {
+    $mobileMenuToggle.click(function () {
         var $this = $(this);
         $this.children().toggleClass('clicked');
         if ($this.children().hasClass('clicked')) {
-            $mobileMenu.slideDown(250, 'easeOutBack');            
+            $mobileMenu.slideDown(250, 'easeOutBack');
         } else {
-            $mobileMenu.slideUp(300);            
+            $mobileMenu.slideUp(300);
         }
-        
+
     });
 
-    $scrollTop.on('click', function() {
+    $scrollTop.on('click', function () {
         $('body, html').animate({
             scrollTop: $('#top').offset().top
         }, 1000);
     });
 
-    $('.mobile-dropdown', $mobileMenu).click(function() {
+    $('.mobile-dropdown', $mobileMenu).click(function () {
         $(this).find('.mobile-submenu').toggleClass('show');
     });
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($mobileMenu.css('display') !== 'none') {
             closeMobileMenu(true);
         }
@@ -85,13 +95,13 @@ $(function() {
         }
     });
 
-    $('#main').click(function() {
+    $('#main').click(function () {
         if ($mobileMenu.css('display') !== 'none') {
             closeMobileMenu(true);
         }
     });
 
-    $(window).resize(function() {
+    $(window).resize(function () {
         if (innerWidth > 750 && $mobileMenu.css('display') !== 'none') {
             closeMobileMenu();
         }
